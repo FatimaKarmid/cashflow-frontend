84984425704485768473f9fd5b755b571d6f74e6
FatimaKarmid <Fatima.Karmid@Student.HTW-Berlin.de>
2025-12-13 10:08:27 +0100

    Commit

--- src/main.js
+++ src/main.js
@@ -1,13 +1,4 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-<<<<<<< HEAD
 createApp(App).mount('#app')
-=======
-Vue.config.productionTip = false
-
-new Vue({
-    render: h => h(App),
-}).$mount('#app')
->>>>>>> 685e666 (Hinzufügt: Formular für neue Ausgaben)
-
